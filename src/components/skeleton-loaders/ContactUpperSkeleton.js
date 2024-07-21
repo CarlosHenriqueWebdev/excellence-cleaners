b1e17c4bb57ea819951e3877bd95fbadd2f0ad86
CarlosHenrique7777 <carloshenrique.webdev@gmail.com>
2024-07-21 13:21:47 -0300

    feat: added right language flags to translation files

--- src/components/skeleton-loaders/ContactUpperSkeleton.js
+++ src/components/skeleton-loaders/ContactUpperSkeleton.js
@@ -3,21 +3,21 @@ export default function ComponentSkeleton() {
     <div className="px-[24px] lg:px-[80px] mx-auto max-w-[640px] md:max-w-full xl:max-w-[1280px] flex flex-col gap-[24px] w-full">
       <div className="flex flex-col gap-[20px]">
         <div className="flex flex-col gap-[16px]">
-          <div className="h-[16px] w-[150px] bg-skeletonLoader rounded"></div>
-          <div className="h-[24px] w-[200px] bg-skeletonLoader rounded"></div>
+          <div className="h-[16px] w-full max-w-[150px] bg-skeletonLoader rounded"></div>
+          <div className="h-[24px] w-full max-w-[200px] bg-skeletonLoader rounded"></div>
         </div>
 
         <div className="flex flex-col gap-[16px]">
-          <div className="h-[16px] w-[100%] bg-skeletonLoader rounded"></div>
-          <div className="h-[16px] w-[100%] bg-skeletonLoader rounded"></div>
+          <div className="h-[16px] w-full bg-skeletonLoader rounded"></div>
+          <div className="h-[16px] w-full bg-skeletonLoader rounded"></div>
         </div>
       </div>
 
       <ul className="flex flex-wrap gap-[16px]">
         {Array.from({ length: 3 }).map((_, index) => (
           <li key={index} className="flex gap-[8px] items-center">
-            <div className="w-[20px] h-[20px] bg-skeletonLoader rounded"></div>
-            <div className="h-[16px] w-[150px] bg-skeletonLoader rounded"></div>
+            <div className="w-full max-w-[20px] h-[20px] bg-skeletonLoader rounded"></div>
+            <div className="h-[16px] w-full max-w-[150px] bg-skeletonLoader rounded"></div>
           </li>
         ))}
       </ul>
