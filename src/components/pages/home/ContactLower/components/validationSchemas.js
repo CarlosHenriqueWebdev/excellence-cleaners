9241f23a8ba22cca44fb3ed40475d6226c278014
CarlosHenrique7777 <carloshenrique.webdev@gmail.com>
2024-07-24 02:16:50 -0300

    fix: added missing validation line for message and address

--- src/components/pages/home/ContactLower/components/validationSchemas.js
+++ src/components/pages/home/ContactLower/components/validationSchemas.js
@@ -4,6 +4,8 @@ export const getValidationSchema = (t) => {
   const baseValidationSchema = Yup.object().shape({
     fullName: Yup.string().required(t("requiredField")),
     email: Yup.string().email(t("invalidEmail")).required(t("requiredField")),
+    address: Yup.string().required(t("requiredField")),
+    message: Yup.string().required(t("requiredField")),
   });
 
   return baseValidationSchema;
