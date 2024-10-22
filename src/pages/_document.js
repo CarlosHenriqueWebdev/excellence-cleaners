5b3247d763171a4fe0fa29fe17738d3552288eb1
Carlos Henrique <carloshenrique.webdev@gmail.com>
2024-10-22 13:19:22 -0300

    feat: placed the GTM and Meta Pixel in the document.js file

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,15 +4,44 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P3QPRQPJ');`,
+          }}
+        />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '1221310782438757');fbq('track', 'PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1221310782438757&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {/* Preload links */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="preload" href="/assets/images/logo.svg" as="image" />
-        <link
-          rel="preload"
-          href="/assets/images/star-cleaners.webp"
-          as="image"
-        />
+        <link rel="preload" href="/assets/images/star-cleaners.webp" as="image" />
       </Head>
       <body className="text-textBlack bg-white">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P3QPRQPJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
